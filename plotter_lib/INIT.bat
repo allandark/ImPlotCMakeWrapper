66a65fb4605b4d6fd14d6bd014544b18498f47c6
dennis jensen <goegler13@gmail.com>
2023-01-06 07:21:41 +0100

    startup script

---
+++ plotter_lib/INIT.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+xcopy ".\cmake files\imgui\CMakelists.txt" imgui
+xcopy ".\cmake files\implot\CMakelists.txt" implot
+
+PAUSE
---
+++ plotter_lib/INIT.sh
@@ -0,0 +1,3 @@
+#bin/bash
+cp cmake\ files/imgui/CMakeLists.txt imgui/
+cp cmake\ files/implot/CMakeLists.txt implot/
